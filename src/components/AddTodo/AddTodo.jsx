e6fc46c117769884c47057cee082164555355210
gomgom35b <gongwon35.b@gmail.com>
2022-11-04 00:54:59 +0900

    Add new list

---
+++ src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+export default function AddTodo({ onAdd }) {
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //trim은 text 자체를 수정한다
+    if (text.trim().length === 0) {
+      return;
+    }
+    onAdd({
+      id: "몰라",
+      text: "text",
+      status: "active",
+    });
+    setText("");
+  };
+  return (
+    //버튼에 따로 이벤트를 주지 않아도 된다!
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add Todo"
+        value={text}
+        onChange={handleChange}
+      ></input>
+      <button>Add</button>
+    </form>
+  );
+}
